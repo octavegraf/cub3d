511c0f9537bc2d37b93266ed9164e95df5c0b4a4
Rémi Chan Renous <rchan-re@student.42.fr>
2025-12-12 18:51:09 +0100

    doors: enable double opening when two doors are close to each other

--- src/doors.c
+++ src/doors.c
@@ -6,83 +6,83 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 12:50:01 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/12/12 14:11:53 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/12/12 18:50:24 by rchan-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifdef BONUS
 # include "cub3d.h"
 
-void	door_hook_2(t_game *game, int x, int y);
-void	door_hook_3(t_game *game, int x, int y);
+static void	check_current_left(char **map, int x, int y)
+{
+	if (map[x][y] == c_l)
+	{
+		map[x][y] = o_l;
+		map[x - 1][y] = d_l_u;
+		map[x + 1][y] = d_l_d;
+	}
+	else if (map[x][y] == o_l)
+	{
+		map[x][y] = c_l;
+		map[x - 1][y] = '1';
+		map[x + 1][y] = '1';
+	}
+}
+
+static void	check_right_left(char **map, int x, int y)
+{
+	if (map[x][y + 1] == c_l)
+	{
+		map[x][y + 1] = o_l;
+		map[x - 1][y + 1] = d_l_u;
+		map[x + 1][y + 1] = d_l_d;
+	}
+	else if (map[x][y + 1] == o_l)
+	{
+		map[x][y + 1] = c_l;
+		map[x - 1][y + 1] = '1';
+		map[x + 1][y + 1] = '1';
+	}
+}
+
+static void	check_current_up(char **map, int x, int y)
+{
+	if (map[x][y] == c_u)
+	{
+		map[x][y] = o_u;
+		map[x][y - 1] = d_u_l;
+		map[x][y + 1] = d_u_r;
+	}
+	else if (map[x][y] == o_u)
+	{
+		map[x][y] = c_u;
+		map[x][y - 1] = '1';
+		map[x][y + 1] = '1';
+	}
+}
+
+static void	check_below_up(char **map, int x, int y)
+{
+	if (map[x + 1][y] == c_u)
+	{
+		map[x + 1][y] = o_u;
+		map[x + 1][y - 1] = d_u_l;
+		map[x + 1][y + 1] = d_u_r;
+	}
+	else if (map[x + 1][y] == o_u)
+	{
+		map[x + 1][y] = c_u;
+		map[x + 1][y - 1] = '1';
+		map[x + 1][y + 1] = '1';
+	}
+}
 
 void	door_hook(t_game *game)
 {
-	int	x;
-	int	y;
-
-	x = game->player.pos_x;
-	y = game->player.pos_y;
-	if (game->scene.map[x][y] == c_l)
-	{
-		game->scene.map[x][y] = o_l;
-		game->scene.map[x - 1][y] = d_l_u;
-		game->scene.map[x + 1][y] = d_l_d;
-	}
-	else if (game->scene.map[x][y + 1] == c_l)
-	{
-		game->scene.map[x][y + 1] = o_l;
-		game->scene.map[x - 1][y + 1] = d_l_u;
-		game->scene.map[x + 1][y + 1] = d_l_d;
-	}
-	else if (game->scene.map[x][y] == c_u)
-	{
-		game->scene.map[x][y] = o_u;
-		game->scene.map[x][y - 1] = d_u_l;
-		game->scene.map[x][y + 1] = d_u_r;
-	}
-	else
-		door_hook_2(game, x, y);
-}
-
-void	door_hook_2(t_game *game, int x, int y)
-{
-	if (game->scene.map[x + 1][y] == c_u)
-	{
-		game->scene.map[x + 1][y] = o_u;
-		game->scene.map[x + 1][y - 1] = d_u_l;
-		game->scene.map[x + 1][y + 1] = d_u_r;
-	}
-	else if (game->scene.map[x][y] == o_l)
-	{
-		game->scene.map[x][y] = c_l;
-		game->scene.map[x - 1][y] = '1';
-		game->scene.map[x + 1][y] = '1';
-	}
-	else if (game->scene.map[x][y + 1] == o_l)
-	{
-		game->scene.map[x][y + 1] = c_l;
-		game->scene.map[x - 1][y + 1] = '1';
-		game->scene.map[x + 1][y + 1] = '1';
-	}
-	else
-		door_hook_3(game, x, y);
-}
-
-void	door_hook_3(t_game *game, int x, int y)
-{
-	if (game->scene.map[x][y] == o_u)
-	{
-		game->scene.map[x][y] = c_u;
-		game->scene.map[x][y - 1] = '1';
-		game->scene.map[x][y + 1] = '1';
-	}
-	else if (game->scene.map[x + 1][y] == o_u)
-	{
-		game->scene.map[x + 1][y] = c_u;
-		game->scene.map[x + 1][y - 1] = '1';
-		game->scene.map[x + 1][y + 1] = '1';
-	}
+	check_current_left(game->scene.map, game->player.pos_x, game->player.pos_y);
+	check_right_left(game->scene.map, game->player.pos_x, game->player.pos_y);
+	check_current_up(game->scene.map, game->player.pos_x, game->player.pos_y);
+	check_below_up(game->scene.map, game->player.pos_x, game->player.pos_y);
 }
 
 #endif
